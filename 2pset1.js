04511034043b72d585444416e243d8824fd394ff
swanandisarpale <swanandisarpale@gmail.com>
2026-08-05 22:25:36 +0530

    added file

---
+++ 2pset1.js
@@ -0,0 +1,21 @@
+const product={
+    title:"ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
+console.log(typeof product);
+
+
+const profile={
+    username:"sanu876",
+    isfollow:false,
+    followers:678,
+    following:900,
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.username);
+
+

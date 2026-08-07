c39300e8ca40b918ab15edbe7da7d8a4c4677b5b
swanandisarpale <swanandisarpale@gmail.com>
2026-08-07 22:40:28 +0530

    added file

---
+++ 4condition.js
@@ -0,0 +1,29 @@
+//if statement
+let age=25;
+if(age>18){
+    console.log(" you can vote");
+}
+let mode="dark";
+let color;
+if(mode=="dark"){
+    color="black";
+}
+console.log(color)
+
+//if else
+    if(mode=="dark"){
+    color="black";
+}else{
+    color="white "
+}
+console.log(color)
+
+//elseif
+if(age>18){
+    console.log("can vote");
+}else if(age<10){
+    console.log("child");
+}else{
+    console.log("cant vote");
+}
+     

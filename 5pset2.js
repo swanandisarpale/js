ea18a9855a313a916090900810f234e9c4ef5ebd
swanandisarpale <swanandisarpale@gmail.com>
2026-08-08 22:23:14 +0530

    added file

---
+++ 5pset2.js
@@ -0,0 +1,26 @@
+//alert("hello");//shows pop up on server
+
+//prompt("hiii");//takes input
+// let  name=prompt("hello");
+// console.log(name);
+
+
+//question1
+// let num=prompt("enter a number");
+// if(num%5==0){
+//     console.log("multiple of 5");
+// }else{
+//     console.log("not a multiplle of 5")
+// }
+
+//q2
+let m=prompt("marks");
+if(m>=90&&m<=100){
+    console.log("a");
+}else if(m>=80&&m<=89){
+    console.log("b");
+}else if(m>=70&&m<=79){
+    console.log("c");
+}else{
+    console.log("fail");
+}
